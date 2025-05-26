0a4d1e742f7320820c04c4e80f0fd6bf1a25b1d9
rafsal3 <rafsaln2001@gmail.com>
2025-05-26 12:57:16 +0530

    nope

--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -1,6 +1,7 @@
 import AddNewModule from '@/components/AddNewModule'
 import FinalFolderComponent from '@/components/contentComponents/FinalFolderComponent'
 import FolderComponent from '@/components/contentComponents/FolderComponent'
+import ImportBtn from '@/components/contentComponents/importBtn'
 import { DataTableDemo } from '@/components/DataTableDemo'
 import { Button } from '@/components/ui/button'
 import { Plus, ScanText } from 'lucide-react'
@@ -9,7 +10,7 @@ import React from 'react'
 const ContentPage = () => {
   return (
     <div className='h-full w-full lg:p-10 p-5'>
-     <div className="h-auto w-full flex flex-wrap gap-4">
+     <div className="h-auto w-full flex flex-wrap gap-4 items-start">
       <FolderComponent/>
       <FolderComponent/>
       <FolderComponent/>
@@ -18,11 +19,10 @@ const ContentPage = () => {
      </div>
      <div className="h-auto mt-10 w-full flex justify-end items-end gap-2">
       <Button>
-        <Plus color='white' /> Create
+        <Plus color='white' /> Create hai
       </Button>
-      <Button className='bg-[#F5F5F5] text-black hover:bg-white'>
-        <ScanText/> Import
-      </Button>
+      <ImportBtn/>
+      
      </div>
      
      <DataTableDemo/>
